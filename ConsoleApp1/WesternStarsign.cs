21943907382d5628733c135a6fe1c12e09436200
Andreas Bergh <andreas__bergh@hotmail.com>
2016-08-30 19:09:26 +0200

    look

---
+++ ConsoleApp1/WesternStarsign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum WesternStarsign
+    {
+        Stenbocken, // 22 december – 20 januari
+        Vattumannen, // 21 januari – 18 februari
+        Fiskarna, // 19 februari – 20 mars
+        Väduren,// 21 mars – 20 april
+        Oxen, // 21 april – 21 maj
+        Tvillingarna, // 22 maj – 21 juni
+        Kräftan, // 22 juni – 22 juli
+        Lejonet, // 23 juli – 23 augusti
+        Jungfrun, // 24 augusti – 22 september
+        Vågen, // 23 september – 23 oktober
+        Skorpionen, // 24 oktober – 22 november
+        Skytten, // 23 november – 21 december
+
+    }
+}
---
+++ ReCapCsharp/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCapCsharp
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        readonly bool ISAdult;
+        readonly string SunSign;
+        readonly string ChineseSign;
+        readonly DateTime Birthday;
+        readonly string ScreenName;
+
+
+
+        public Person(string firstname, string lastname, string email, DateTime dateofbirth)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            DateOfBirth = dateofbirth;
+        }
+
+        public Person(string firstname, string lastname, string email)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+        }
+        public Person(string firstname, string lastname, DateTime dateofbirth)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            DateOfBirth = dateofbirth;
+        }
+    }
+}
